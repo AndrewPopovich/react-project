2d74422441426882a168a27f04728c4085eac8f6
AndrewPopovich <andrew1994popovich@gmail.com>
2019-02-14 23:37:13 +0200

    Move to parent package

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export  default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Hi, I`m Andrew</h1>
+      </div>
+    );
+  }
+}
