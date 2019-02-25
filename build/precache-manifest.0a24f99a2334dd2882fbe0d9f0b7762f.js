2faf5153492d9085b0e03ae2132975bf1d68477c
AndrewPopovich <andrew1994popovich@gmail.com>
2019-02-25 23:32:22 +0200

    commit for css-modules

---
+++ build/precache-manifest.0a24f99a2334dd2882fbe0d9f0b7762f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65516530fb87a09ab113",
+    "url": "/static/css/main.0dcb86f5.chunk.css"
+  },
+  {
+    "revision": "65516530fb87a09ab113",
+    "url": "/static/js/main.65516530.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "7dd0ee7ad835ebbada3021cca8696a85",
+    "url": "/index.html"
+  }
+];
